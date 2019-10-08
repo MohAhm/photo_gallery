797381af001b59ccd889417ac1fc0427b4d85a8c
Mohamed Ahmed <a.mohamed.cali@gmail.com>
2019-10-08 22:05:58 +0200

    Style Lightbox 2 plugin

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-// alert('Hello!');
+// Lightbox
+lightbox.option({
+    'alwaysShowNavOnTouchDevices': true,
+    'resizeDuration': 100,
+    'wrapAround': true,
+    'fadeDuration': 100,
+    'showImageNumberLabel': false,
+    'maxWidth': 900,
+    'positionFromTop': 130,
+    'disableScrolling': true
+});
