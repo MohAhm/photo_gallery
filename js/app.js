38536da46bcee3c547d3f8c3f70c0b6b31579e74
Mohamed Ahmed <a.mohamed.cali@gmail.com>
2019-10-13 18:42:10 +0200

    Add filtering functionality

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Lightbox
+// Lightbox Functionality
 lightbox.option({
     'alwaysShowNavOnTouchDevices': true,
     'resizeDuration': 100,
@@ -8,4 +8,28 @@ lightbox.option({
     'maxWidth': 900,
     'positionFromTop': 130,
     'disableScrolling': true
+});
+
+
+// Content Filtering Functionality
+$('#search').on('keyup', function () {
+
+    const inputValue = $(this).val().toLowerCase();
+    // console.log(inputValue);
+
+    $('.photo a').each(function () {
+
+        const caption = $(this).attr('data-title').toLowerCase();
+        // console.log($caption);
+
+        // attr() and removeAttr() are used to prevent unmatch images to be navigated on Lightbox
+        if (caption.includes(inputValue)) {
+            $(this).parent().show();
+            $(this).attr('data-lightbox', 'photo-gallery');
+        } else {
+            $(this).parent().hide();
+            $(this).removeAttr('data-lightbox');
+        }
+    });
+
 });
